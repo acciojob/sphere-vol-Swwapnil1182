b4a362b90a457ac69551e784149ba8b08ccdfcc8
Swwapnil1182 <swapniljadhav1182@gmail.com>
2025-05-15 20:55:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function volume_sphere() {
 	const volume=document.getElementById("volume")
 
 		if (isNaN(radius) || radius < 0) {
-		volumeField.value = "NaN";
+		volume.value = "NaN";
 		return;
 	}
 	const volres= (4 / 3) * Math.PI * Math.pow(radius, 3);
